26b0a76c465593eb660d369b09e406829b899cd2
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-09-16 15:55:31 +1000

    More changes

--- upload.py
+++ upload.py
@@ -29,7 +29,7 @@ filenames = [
 username = 'faimsadmin@intersect.org.au'
 password = 'Pass.123'
 
-server = 'dev'
+server = 'faims2'
 url    = 'http://%s.fedarch.org' % server
 
 ################################################################################
@@ -53,7 +53,7 @@ else:
 moduleName = moduleLocation.split(os.sep)
 if moduleName[-1] == 'module': moduleName = moduleName[-2]
 else:                          moduleName = moduleName[-1]
-moduleName = 'Spit Excavation Blue Mountains Development'
+moduleName = 'Spit Excavation Blue Mountains'
 
 # Check that all the given paths really exist
 if not os.path.exists(moduleLocation):
