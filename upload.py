34ba199d284a460920b88df23e68578052f7cb23
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-09-16 13:52:23 +1000

    Improved identifiers, added search

--- upload.py
+++ upload.py
@@ -53,7 +53,7 @@ else:
 moduleName = moduleLocation.split(os.sep)
 if moduleName[-1] == 'module': moduleName = moduleName[-2]
 else:                          moduleName = moduleName[-1]
-moduleName = 'Spit Excavation Blue Mountains'
+moduleName = 'Spit Excavation Blue Mountains Development'
 
 # Check that all the given paths really exist
 if not os.path.exists(moduleLocation):
